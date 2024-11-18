0b2b0929112f9e24522ca3ef127f89f42780370c
Jonathan Martin <jonnymartin93@gmail.com>
2024-11-18 13:46:04 -0600

    proposed changes to allow for script to be run as is and allow admins to leverage whatever user they would like

--- bin/microk8s_installer.sh
+++ bin/microk8s_installer.sh
@@ -1,8 +1,9 @@
-#!/bin/bash KLG - Microk8s installer script
+#!/bin/bash
+# KLG - Microk8s installer script
 
 echo "This will install Microk8s version 1.26, setup current user for kubes and create the alias for kubectl"
 sudo snap install microk8s --classic --channel=1.26/stable
-sudo usermod -a -G microk8s splunker
-sudo chown -f -R splunker ~/.kube
+sudo usermod -a -G microk8s $SUDO_USER
+sudo chown -f -R $SUDO_USER ~/.kube
 sudo snap alias microk8s.kubectl kubectl
 echo "logout now and back in to continue setup"
