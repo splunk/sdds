5a5662a09b0212bf5529bc8cff669a1d5594e799
Kate Lawrence-Gupta <39601928+klawrencegupta-splunk@users.noreply.github.com>
2025-02-20 13:38:10 -0500

    updates
    updates microk8s to latest stable version
    changes restmap.conf to default deployment
    adding optional server.conf if the customer needs to set the DS to a specific value used in production already

--- bin/microk8s_installer.sh
+++ bin/microk8s_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash KLG - Microk8s installer script
 
-echo "This will install Microk8s version 1.26, setup current user for kubes and create the alias for kubectl"
-sudo snap install microk8s --classic --channel=1.26/stable
+echo "This will install Microk8s version 1.32, setup current user for kubes and create the alias for kubectl"
+sudo snap install microk8s --classic --channel=1.32/stable
 sudo usermod -a -G microk8s splunker
 sudo chown -f -R splunker ~/.kube
 sudo snap alias microk8s.kubectl kubectl
